307ed176a1d4349a36e5ac1249555951ac361deb
phamhai12412 <128035449+phamhai12412@users.noreply.github.com>
2023-03-28 07:54:04 +0700

    Add files via upload

---
+++ bt.js
@@ -0,0 +1,41 @@
+//bt 1-4
+// let msg = "console.log(c);";
+// console.log(msg);
+// let studentCount = 0;
+// console.log(studentCount);
+// msg = "'Coding is hard but super cool and fun'";
+// console.log(msg);
+// studentCount = 30;
+// console.log(studentCount);
+// console.log(msg.toLocaleUpperCase());
+// let n = prompt("nhập vào giá trị n");
+
+// console.log(studentCount + Number(n));
+//bt5
+// alert("hello anh bách đẹp trai");
+//bt6
+// let ten = prompt("nhập vào tên của bạn");
+// alert(`xin chào bạn: ${ten}`);
+//bt7
+// let ho = prompt("nhập vào họ của bạn:");
+// let ten = prompt("nhập vào tên của bạn");
+// alert(`họ và tên của bạn là:${ho} ${ten}`);
+//bt8
+// let a = prompt("nhập vào cạnh hình vuông");
+// let dtich = a * a;
+// alert(` diện tích hình vuông là:${dtich}`);
+//bt9
+// let r = prompt("nhập vào bán kính hình tròn");
+// let tron = r * r * 3.14;
+// alert(`diện tích hình tròn là:${tron}`);
+//bt10
+// let c = prompt("nhập vào độ c:");
+// let f = c * 1.8 + 32;
+// alert(` ${c} độ c=${f} độ f`);
+//bt11
+// alert(`The quick ___ fox jumps over the ___ dog`);
+// let t1 = prompt("điền vào chỗ trống thứ 1 từ:");
+// let t2 = prompt("điền vào chỗ trống thứ 2 từ:");
+// alert(`câu bạn đã diền: The quick ${t1} fox jumps over the ${t2} dog `);
+//lms tùy biến màu console
+// console.log("%cThis Is Line One ", "color: red;");
---
+++ toantu.js
@@ -0,0 +1,32 @@
+//toán tử số học gồm +,-,*,/,**,%,++,--
+let a, b;
+a = 1;
+b = 2;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a ** a);
+console.log(a % b);
+console.log(++a);
+console.log(b--)
+//toán tử so sánh: ==,===,<=,>=,<,>,!=,!==
+let c = 1;
+let d = '1';
+console.log(c == d)
+console.log(c === d)
+console.log(c >= d)
+console.log(c <= d)
+console.log(c < d)
+console.log(c > d)
+console.log(c != d)
+console.log(c !== d)
+//toán tử 3 ngôi
+a > b ? "a lớn hơn b" : "a nhỏ hơn b"
+//toán tử logic: &&,||,!
+//toán tử gán:=,+=,-=,*=,/=,%=,**=
+//a=a+b sẽ bằng a+=b tương tự với các trường hợp khác
+
+
+//khác nhau giữa == và === 1 bên so sánh bằng theo giá trị, 1 bên so sánh cả kiểu dữ liệu và giá trị
+//so sánh tương đối và so sánh tuyệt đối
